f50782770e4c136ed5f0942c6fed1c6f7c31621e
Allan Hebert <hebertbr.allan@outlook.com>
2025-06-29 10:13:33 -0300

    commit dos arquivos funcionais

---
+++ index.js
@@ -0,0 +1,13 @@
+const {Client,  LocalAuth} = require('whatsapp-web.js')
+const qrcode = require("qrcode-terminal")
+
+
+const client = new Client({authStrategy: new LocalAuth()})
+
+
+client.on('qr', qr=>qrcode.generate(qr, {small:true}))
+
+client.on('ready', ()=>console.log("whatsapp totalmente pronto para uso"))
+
+
+client.initialize()
