d08fda34cb220da210007cb77f01d303ae442a72
Allan Hebert <hebertbr.allan@outlook.com>
2025-06-29 10:27:33 -0300

    correção do puppeteer

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const {Client,  LocalAuth} = require('whatsapp-web.js')
 const qrcode = require("qrcode-terminal")
 
 
-const client = new Client({authStrategy: new LocalAuth()})
+const client = new Client({
+    authStrategy: new LocalAuth(), // mantém sessão salva localmente
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }})
 
 
 client.on('qr', qr=>qrcode.generate(qr, {small:true}))
